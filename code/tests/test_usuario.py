7996fd20945009989df9c25680748955121e4eee
Beatriz Cardozo <beatriz.a.cardozo28@gmail.com>
2025-09-13 19:45:40 -0300

    test: corrigindo erro de sintaxe do teste

--- code/tests/test_usuario.py
+++ code/tests/test_usuario.py
@@ -1,6 +1,6 @@
 import pytest
 
-from app.usuario import Usuario
+from app.usuario.Usuario import Usuario
 
 def test_criar_conta_cep_padrao(): # CEP completamente dentro do padrão XXXXX-XXX
     novo_usuario = Usuario(
@@ -39,25 +39,26 @@ def test_criar_conta_cep_valido_invalido(): # CEP válido (8 digitos numericos) 
         "Brasileira"
     )
 
-    assert novo_usuario.criar_conta() == "CEP invalido"
+    assert novo_usuario.criar_conta() == "Erro ao criar conta"
 
 def test_idade_invalida():
-        novo_usuario = Usuario(
+    novo_usuario = Usuario(
         "Beatriz",
         "beatriz.cardozo@ges.inatel.br",
         "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cac",
         "1234569999978",
-        -5
+        -5,
         "28-05-2003",
         "Brasileira"
     )
     assert novo_usuario.criar_conta() == "Erro ao criar conta"
 
 def test_faltando_dados():
+    
     novo_usuario = Usuario(
         "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cac",
         "1234569999978",
-        -5
+        28,
         "28-05-2003",
         "Brasileira"
     )
