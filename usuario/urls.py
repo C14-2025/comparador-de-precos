feeeeb4f3dd4d687b6203469b3e9a67c9d489501
Vinícius Carvalho Ensá <160641240+ViniciusCarvalhoEnsa@users.noreply.github.com>
2025-11-26 15:05:51 -0300

    feat: Adicionando projeto Django

---
+++ usuario/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+# from usuario.views import cadastro, login # Importa as funções html da aplicação
+
+# urlpatterns = [
+#     path('cadastro', cadastro, name='cadastro'),
+#     path('login', login, name='login'),
+# ]
---
+++ usuario/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Responsável por cuidar do que vai ser exibido
+# Create your views here.
+
+# def cadastro(request): # Função para a página de cadastro da aplicação usuario
+#     return render(request, 'comparador_de_preco/usuario/cadastro.html')
+
+# def login(request): # Função para a página de login da aplicação usuario
+#     return render(request, 'comparador_de_preco/usuario/login.html')
+
+
+
+# from django.http import HttpResponse # SÓ DE BRINCADEIRA PRA TESTAR
+
+# def teste(request):
+#     return HttpResponse('<h1>Bom dia para:</h1><p>Bia, Fefe, John, Chockito, China e o grande Vinnie!</p>')
