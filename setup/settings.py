643bb3cd5918335f1585df7f54a3fc7d9bece4d4
Vinícius Carvalho Ensá <160641240+ViniciusCarvalhoEnsa@users.noreply.github.com>
2025-11-26 15:40:39 -0300

    feat: Adicionando projeto Django - Criação do project - Criação das aplicações 'usuario' e 'produto' - Configuração do settings.py - Criação do arquivo .env, requirements.txt e git ignore - Criação da estrutura de templetes - Criação de templates básicos - Criação da pasta static para configuração de arquivos static

--- setup/settings.py
+++ setup/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY')) # Utiliza a Secret Key nas variáveis de ambiente
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
