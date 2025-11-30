0a8a589965eb06ccf7799cacd5122f3765ccc55c
Felipe Ferreira <felipefcgp.se@gmail.com>
2025-11-30 20:45:31 -0300

    refactor: Movendo classes craler para o arquivo crawler.py

--- buscaCrawler/crawling/mercado_livre.py
+++ buscaCrawler/crawling/mercado_livre.py
@@ -1,24 +1,2 @@
-import requests
-from bs4 import BeautifulSoup
-import urllib.parse
-from crawling.base import BaseCrawler
 
-class MercadoLivreCrawler(BaseCrawler):
-    def search(self, query: str):
-        q = urllib.parse.quote(query)
-        url = f"https://lista.mercadolivre.com.br/{q}"
 
-        headers = {
-            "User-Agent": "Mozilla/5.0",
-            "Accept-Language": "pt-BR,pt;q=0.9"
-        }
-
-        r = requests.get(url, headers=headers)
-        soup = BeautifulSoup(r.text, "html.parser")
-
-        selector = "li.ui-search-layout__item"
-        products = soup.select(selector)[:10]
-
-        results = [{"html": str(p)} for p in products]
-        self.save_json(results, "mercado_livre")
-        return results
