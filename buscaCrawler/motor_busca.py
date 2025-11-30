6db1b114d8841ee7be9b98b7f6329a77964902b1
Felipe Ferreira <felipefcgp.se@gmail.com>
2025-11-30 19:57:57 -0300

    feat: Adicionando link e verificação de frete gratis

--- buscaCrawler/motor_busca.py
+++ buscaCrawler/motor_busca.py
@@ -2,6 +2,7 @@ import json
 import os
 import glob
 import re
+import unicodedata
 
 from crawling.amazon import AmazonCrawler
 from crawling.mercado_livre import MercadoLivreCrawler
@@ -12,7 +13,8 @@ from refactor_produto import Produto
 
 
 class MotorDeBusca:
-    PATH_TEMP = 'app/json_temp'
+    # Mudar Path
+    PATH_TEMP = 'app/json_temp/'
     
     def busca(produto: str):
         AmazonCrawler().search(query=produto)
@@ -24,16 +26,17 @@ class MotorDeBusca:
         dados_amazon = MotorDeBusca.carregar_dados_json(caminho_amazon)
         dados_mlivre = MotorDeBusca.carregar_dados_json(caminho_mlivre)
 
-        if dados_mlivre:
-            produtos += MotorDeBusca.tratando_mercado_livre(dados_mlivre)
-        
-        if dados_amazon:
-            produtos += MotorDeBusca.tratando_amazon(dados_amazon)
+        produtos = []
 
-        MotorDeBusca.limpar_arquivos_temporarios()
+        produtos += MotorDeBusca.tratando_mercado_livre(dados_mlivre)
+    
+        produtos += MotorDeBusca.tratando_amazon(dados_amazon)
+
+        #MotorDeBusca.limpar_arquivos_temporarios()
         
         return produtos
 
+    #remover funcao
     @staticmethod
     def limpar_arquivos_temporarios():
         """Apaga todos os JSONs da pasta temporária."""
@@ -48,6 +51,7 @@ class MotorDeBusca:
             except OSError as e:
                 print(f"Erro ao remover {arquivo}: {e}")
 
+    #remover funcao
     @staticmethod
     def carregar_dados_json(caminho_arquivo: str) -> list:
         """Método auxiliar apenas para ler o arquivo."""
@@ -59,20 +63,33 @@ class MotorDeBusca:
             return []
 
     @staticmethod
+    def tem_gratis(texto):
+        texto_lower = texto.lower()
+        
+        nfkd = unicodedata.normalize('NFKD', texto_lower)
+        
+        texto_limpo = "".join([c for c in nfkd if not unicodedata.combining(c)])
+        
+        return "gratis" in texto_limpo
+    
+    @staticmethod
     def tratando_mercado_livre(dados: list) -> list:
-        """Método responsavel por Tratar o HTML da loja 'Mercado Livre' recebido, transformando em objetos da classe Produto"""
-
         produtos = []
 
         for dado in dados:
             soup = BeautifulSoup(dado.get('html', ''), 'html.parser')
 
-            # ======================== NOME ========================
+            # ======================== LINK E NOME =================
             tag_nome = soup.find('a', class_='poly-component__title')
-            nome_produto = tag_nome.text.strip() if tag_nome else "Não encontrado"
             
-            # ======================== PRECO =======================
-            # TODO: CONVERTER MOEDA PARA O PADRAO (3 letras)
+            nome_produto = "Não encontrado"
+            link_produto = "Link não encontrado"
+
+            if tag_nome:
+                nome_produto = tag_nome.text.strip()
+                link_produto = tag_nome.get('href')
+
+            # ======================== VALOR =======================
             tag_moeda = soup.find('span', class_='andes-money-amount__currency-symbol')
             moeda_produto = tag_moeda.text.strip() if tag_moeda else ""
 
@@ -82,8 +99,6 @@ class MotorDeBusca:
             tag_centavos = soup.find('span', class_='andes-money-amount__cents')
             preco_centavos_produto = tag_centavos.text.strip() if tag_centavos else "00"
 
-            # Remove pontos de milhar (ex: 1.200 -> 1200) para conversão correta
-            # O Mercado Livre separa visualmente, mas precisamos de um float limpo
             preco_inteiro_limpo = preco_inteiro_produto.replace('.', '')
             
             string_valor = f"{preco_inteiro_limpo}.{preco_centavos_produto}"
@@ -93,8 +108,26 @@ class MotorDeBusca:
             except ValueError:
                 preco_final_produto = 0.0
 
+            # ======================== AVALIACAO ===================
+            tag_review = soup.find('span', class_='poly-component__review-compacted')
+            nota_produto = 0.0
+            vendas_produto = "0"
+            
+            if tag_review:
+                itens_review = tag_review.find_all('span', class_='poly-phrase-label')
+                
+                if len(itens_review) > 0:
+                    try:
+                        nota_produto = float(itens_review[0].text.strip())
+                    except ValueError:
+                        nota_produto = 0.0
+                
+                if len(itens_review) > 1:
+                    vendas_produto = itens_review[1].text.replace('|', '').strip()
+
             # ======================== FRETE =======================
             tag_frete_rapido = soup.find('span', class_='poly-shipping--next_day')
+
 
             frete_produto = ''
             if tag_frete_rapido:
@@ -102,22 +135,29 @@ class MotorDeBusca:
             else:
                 tag_frete_normal = soup.find('div', class_='poly-component__shipping')
                 if tag_frete_normal:
-                    frete_produto = tag_frete_normal.text.strip()
+                    frete_produto = tag_frete_normal.text.strip()                    
                 else:
                     frete_produto = "Frete Indisponível"
-
+            
+            frete_gratis = False
+            if MotorDeBusca.tem_gratis(frete_produto):
+                frete_gratis = True
+                
             produto = Produto(
                 loja="Mercado Livre",
+                link_produto= link_produto,
                 nome=nome_produto,
                 moeda=moeda_produto,
                 preco=preco_final_produto,
-                frete=frete_produto                    
+                frete=frete_produto,
+                frete_gratis = frete_gratis,
+                nota=nota_produto,
+                numero_vendas=vendas_produto
             )
 
             produtos.append(produto)
     
         return produtos
-
 
     @staticmethod
     def tratando_amazon(dados: list) -> list:
@@ -128,9 +168,23 @@ class MotorDeBusca:
         for dado in dados:
             soup = BeautifulSoup(dado.get('html', ''), 'html.parser')
 
-            # ======================== NOME ========================
+            # ======================== NOME E LINK ========================
             tag_nome = soup.find('h2')
-            nome_produto = tag_nome.text.strip() if tag_nome else "Nome não encontrado"
+            nome_produto = "Nome não encontrado"
+            link_produto = "Link não encontrado"
+
+            if tag_nome:
+                nome_produto = tag_nome.text.strip()
+                
+                tag_link = tag_nome.find_parent('a')
+                
+                if tag_link and tag_link.get('href'):
+                    href_bruto = tag_link.get('href')
+                    
+                    if href_bruto.startswith('/'):
+                        link_produto = f"https://www.amazon.com.br{href_bruto}"
+                    else:
+                        link_produto = href_bruto
 
             # ======================== PRECO =======================
             tag_preco = soup.find('span', class_='a-offscreen')
@@ -169,17 +223,49 @@ class MotorDeBusca:
             tag_frete = soup.find('div', class_='udm-primary-delivery-message')
             if tag_frete:
                 frete_produto = " ".join(tag_frete.text.split())
-            else:
+            else:                
                 frete_produto = "Frete Indisponível. Consultar no Site"
+
+            frete_gratis = False
+            if MotorDeBusca.tem_gratis(frete_produto):
+                frete_gratis = True
+
+            # ======================== NOTA / AVALIAÇÃO =======================
+            tag_nota = soup.find('span', class_='a-icon-alt')
+            nota_produto = 0.0
+            if tag_nota:
+                try:
+                    # Ex: "4,8 de 5 estrelas" -> Pega 4.8 e garante ponto flutuante
+                    texto_nota = tag_nota.text.strip().split(' ')[0].replace(',', '.')
+                    nota_produto = float(texto_nota)
+                except (ValueError, IndexError):
+                    nota_produto = 0.0
+
+            # ======================== VENDAS (Mês Passado) =======================
+            # Ex: "50+ compras no mês passado" ou "100+ bought in past month"
+            vendas_produto = "0"
+            tags_vendas = soup.find_all('span', class_='a-size-base a-color-secondary')
+            
+            for t in tags_vendas:
+                texto_venda = t.text.lower()
+                if 'compra' in texto_venda or 'bought' in texto_venda:
+                    vendas_produto = t.text.strip()
+                    break
+
 
             produto = Produto(
                 loja="Amazon",
+                link_produto=link_produto,
                 nome=nome_produto,
                 moeda=moeda_produto,
                 preco=valor_numerico,
-                frete=frete_produto
+                frete=frete_produto,
+                frete_gratis=frete_gratis,
+                nota=nota_produto,
+                numero_vendas=vendas_produto
             )
             produtos.append(produto)
             
         return produtos
+    
     
